6a09311e3192863368aabff9c5b86ef0b6f332b5
Ghilmanzufar <ghilmanzufar2004@gmail.com>
2025-02-27 20:49:39 +0700

    menambahkan apakah url foto ada atau tidak

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -16,6 +16,7 @@ export function CardProfileComponent() {
         const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
         if (loggedUser) {
             setUser(loggedUser);
+            console.log("User Data:", loggedUser);
             setEditedUser({
                 nama: loggedUser.nama,
                 nomer: loggedUser.nomer,
@@ -135,7 +136,7 @@ export function CardProfileComponent() {
                             </dd>
                         </div>
                         <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt className="text-sm font-medium text-gray-500">photo Profil</dt>
+                            <dt className="text-sm font-medium text-gray-500">Photo Profil</dt>
                             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                                 {isEditing ? (
                                     <input
